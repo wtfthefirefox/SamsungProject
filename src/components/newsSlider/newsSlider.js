ac0769da6cbd7c4f27a288c490830fe44e7ec0af
thefirefox <wtfthefirefox@gmail.com>
2020-09-29 22:41:55 +0300

    animted news slider + added some photos

--- src/components/newsSlider/newsSlider.js
+++ src/components/newsSlider/newsSlider.js
@@ -1,31 +1,60 @@
 import React from 'react';
 
 const NewsSlider = () => {
-    return (
-        <section className="section news">
-        <div className="container news__container">
-          <div className="wrapper news__wrapper">
-            <div className="news__slider">
-              <ul className="news__slider__list-items">
-                <li className="news__slider-item news__slider-item-active galaxyZFlip">
-                </li>
-              </ul>
-              <button className="news__slider__btn news__slider__btn-left"></button>
-              <button className="news__slider__btn news__slider__btn-right"></button>
-              <ul className="news__slider__list-points">
-                <li className="news__slider-points--item points--active"></li>
-                <li className="news__slider-points--item"></li>
-                <li className="news__slider-points--item"></li>
-                <li className="news__slider-points--item"></li>
-                <li className="news__slider-points--item"></li>
-                <li className="news__slider-points--item"></li>
-                <li className="news__slider-points--item"></li>
-              </ul>
-            </div>
+  const onChangeNewsSlider = (sign) => {
+    let catalogItems = document.querySelector('.news__slider__list-items');
+
+    if (!catalogItems.style.transform) {
+      if (sign === '+') {
+          catalogItems.style.transform += `translateX(0px)`;
+      } else {
+          catalogItems.style.transform += `translateX(${sign === '-' ? '-': ''}1366px)`; 
+      }
+    } else {
+      catalogItems.style.transform.split(' ').forEach(item => {
+          if (item.includes('translateX')) {
+              let transformValue = sign === '-' ? parseInt(item.slice(11, item.length - 3), 10) - 1366 : parseInt(item.slice(11, item.length - 3), 10) + 1366
+
+              if (transformValue <= 0 && Math.abs(transformValue) / 1366 < document.querySelectorAll('.news__slider-item').length) {
+                  catalogItems.style = `transform: translateX(${transformValue}px)`;
+              } 
+          }
+      });
+    }
+  }
+
+  return (
+      <section className="section news">
+      <div className="container news__container">
+        <div className="wrapper news__wrapper">
+          <div className="news__slider">
+            <ul className="news__slider__list-items">
+              <li className="news__slider-item" >
+                <img src="../../../assets/img/36Gy2aThDCQVXxbX7yyRtF.jpg" />
+              </li>
+              <li className="news__slider-item">
+                <img src="../../../assets/img/samsungWatch3.png" />
+              </li>
+              <li className="news__slider-item">
+                <img src="../../../assets/img/s20+.jpg" />
+              </li>
+            </ul>
+            <button className="news__slider__btn news__slider__btn-left" onClick={() => onChangeNewsSlider('+')}></button>
+            <button className="news__slider__btn news__slider__btn-right" onClick={() => onChangeNewsSlider('-')}></button>
+            <ul className="news__slider__list-points">
+              <li className="news__slider-points--item points--active"></li>
+              <li className="news__slider-points--item"></li>
+              <li className="news__slider-points--item"></li>
+              <li className="news__slider-points--item"></li>
+              <li className="news__slider-points--item"></li>
+              <li className="news__slider-points--item"></li>
+              <li className="news__slider-points--item"></li>
+            </ul>
           </div>
         </div>
-      </section>
-    )
+      </div>
+    </section>
+  )
 }
 
 export default NewsSlider;
